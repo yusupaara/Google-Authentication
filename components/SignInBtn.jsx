d0f839fe9fff9faef6347e016d96c14cca0a0ff6
yusuplusminus <yoesoep17@gmail.com>
2024-01-26 16:01:34 +0700

    update lib

--- components/SignInBtn.jsx
+++ components/SignInBtn.jsx
@@ -9,7 +9,7 @@ export default function SignInBtn() {
       onClick={() => signIn("google")}
       className="flex items-center gap-4 shadow-xl rounded-lg pl-3"
     >
-      <Image src="/google-logo.png" height={30} width={30} />
+      <Image src="/google-logo.png" alt="google" height={30} width={30} />
       <span className="bg-blue-500 text-white px-4 py-3">
         Sign in with Google
       </span>
